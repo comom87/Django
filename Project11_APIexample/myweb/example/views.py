ac2af070675940de3596fadb50179e4af656f320
cosmos-1885 <dmsrkql104321@gmail.com>
2023-08-14 15:00:16 +0900

    feat: update APIexample

--- Project11_APIexample/myweb/example/views.py
+++ Project11_APIexample/myweb/example/views.py
@@ -11,16 +11,18 @@ from rest_framework.generics import get_object_or_404
 def HelloAPI(request):
     return Response("hello world!")
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST']) # GET/POST 요청을 처리하게 만들어주는 데코레이터
 def booksAPI(request):
     if request.method == 'GET':
         books = Book.objects.all()
+        # 시리얼라이저에 전체 데이터를 한 번에 집어넣기 -> 직렬화
+        # many=True: 여러 데이터에 대한 처리를 한도록 한다.
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        serializer = BookSerializer(data=request.data) # POST 요청으로 들어온 데이터를 시리얼라이저에 집어넣기 -> 역직렬화
+        if serializer.is_valid(): # 들어온 데이터가 모델에 맞는 유효한 데이터라면
+            serializer.save() # serailizer.save(): 기본적인 create() 함수를 실행시키는 모델시리얼라이저의 기능
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -49,8 +51,8 @@ class BookAPI(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    queryset = Book.objects.all() # queryset: 모델에 질문을 보내 받아온 데이터가 들어간다. 일반적으로 그냥 모든 데이터를 불러온다.
+    serializer_class = BookSerializer # serializer_class: 해당 API에서 사용할 시리얼라이저를 설정한다.
     
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
@@ -60,7 +62,7 @@ class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Ge
 class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    lookup_field = 'bid'
+    lookup_field = 'bid' # Django 기본 모델의 기본키가 아니라 bid를 기본키로 사용하고 있으므로 lookup_field를 설정한다.
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -76,4 +78,4 @@ class BooksAPIGenerics(generics.ListCreateAPIView):
 class BookAPIGenerics(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    lookup_field = 'bid'
+    lookup_field = 'bid' 
